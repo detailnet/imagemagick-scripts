97e60182435a37c22a170d272b501b4c58342dc4
Ivan Wolf <iwolf@detailag.ch>
2015-02-24 08:17:29 +0100

    Added first version of image conversion script

---
+++ convert_image.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#set -x
+
+input_file="$1"
+output_file="$2"
+profile_file="profile.icc"
+srgb_profile_file="sRGB.icm"
+size="200"
+
+convert "$input_file" "$profile_file"
+
+if [ $? -eq 0 ]
+then
+  echo "Color profile found"
+  convert -profile $profile_file $input_file -profile $srgb_profile_file -thumbnail $size\x$size\> -density 72 $output_file
+else
+  echo "No color profile found"
+  convert $input_file -profile $srgb_profile_file -thumbnail $sizex$size\> -density 72 $output_file
+fi
+
+if [ $? -eq 0 ]
+then
+  echo "Successfully converted image and saved to $output_file"
+else
+  echo "No color profile found"
+  echo "Failed to convert image"
+fi
+
+exit $?
