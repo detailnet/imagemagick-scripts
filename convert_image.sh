b4d892271cea17dcd434b5c0593e93d4dbe08890
Curzio Della Santa <cdellasanta@detailag.ch>
2015-02-24 10:04:58 +0100

    Refactured code, added usage, added size and density options

--- convert_image.sh
+++ convert_image.sh
@@ -1,29 +1,143 @@
 #!/bin/bash
-#set -x
 
-input_file="$1"
-output_file="$2"
-profile_file="profile.icc"
-srgb_profile_file="sRGB.icm"
-size="200"
+function usage() {
+	echo ""
+	echo "$(basename $0) - Convert images"
+	echo ""
+	echo "Usage: $(basename $0) -i input_file [options] [convert options]"
+	echo ""
+	echo "options:"
+	echo "-h, --help               Show brief help"
+	echo "-v, --verbose            Verbose output" # chenge to debug
+ 	echo "-i, --input              Input file. Mandatory"
+ 	echo "-o, --output             Output file. Default -"
+ 	echo "-s, --size               Thumbnail size. Default 200x200>" # http://www.imagemagick.org/script/command-line-processing.php#geometry
+ 	echo "-d, --density            Density. Default 72"
+	echo ""
+	echo "convert options: see 'convert --help'"
+	echo ""
+}
 
-convert "$input_file" "$profile_file"
+# Set defaults
+PROFILE_FILE="profile.icc"
+SRGB_PROFILE_FILE="sRGB.icm"
+SIZE="200x200>"
+DENSITY="72"
+OUTPUT_FILE="-"
 
-if [ $? -eq 0 ]
-then
-  echo "Color profile found"
-  convert -profile $profile_file $input_file -profile $srgb_profile_file -thumbnail $size\x$size\> -density 72 $output_file
-else
-  echo "No color profile found"
-  convert $input_file -profile $srgb_profile_file -thumbnail $sizex$size\> -density 72 $output_file
+# Check ImageMagick convert
+CONVERT=$(type -P convert)  || { echo "Script requires ImageMagick's convert but it's not installed."; exit 1; }
+#BC=$(type -P bc)  || { echo "Script requires the binary calculator 'bc' but it's not installed."; exit 1; }
+
+if [[ $# -eq 0 ]]; then
+	usage
+	exit 0
 fi
 
-if [ $? -eq 0 ]
+# Get options
+while test $# -gt 0; do
+	case "$1" in
+		-h|--help)
+			usage
+			exit 0
+			;;
+		-v|--verbose)
+		    shift
+		    set -x
+            ;;
+        -i|--input)
+            shift
+            if test $# -gt 0; then
+                INPUT_FILE=$1
+            else
+            	echo "No input file given."
+            	usage
+            	exit 1
+            fi
+			shift
+			;;
+        -o|--output)
+            shift
+            if test $# -gt 0; then
+                OUTPUT_FILE=$1
+            else
+            	echo "No output file given."
+            	usage
+            	exit 1
+            fi
+			shift
+			;;
+		-s|--size)
+		    shift
+            if test $# -gt 0; then
+                SIZE=$1
+            else
+            	echo "No size given."
+            	usage
+            	exit 1
+            fi
+			shift
+		    ;;
+		-d|--density)
+		    shift
+            if test $# -gt 0; then
+                DENSITY=$1
+            else
+            	echo "No density given."
+            	usage
+            	exit 1
+            fi
+			shift
+		    ;;
+		*)
+			break
+			;;
+	esac
+done
+
+REMAINING=$@
+
+
+# Check that input file is set
+if [[ "AAA${INPUT_FILE}AAA" == "AAAAAA" ]]; then
+	echo "No input file defined. (mandatory)"
+	usage
+	exit 1
+fi
+
+
+# Test if profile is given
+${CONVERT} ${INPUT_FILE} ${PROFILE_FILE} 2>/dev/null
+HAS_PROFILE=$?
+
+# Build convert command, not order of commands is important for convert
+COMMAND="${CONVERT}"
+
+if [ ${HAS_PROFILE} -eq 0 ]
 then
-  echo "Successfully converted image and saved to $output_file"
+  echo "Color profile found"
+
+  COMMAND="${COMMAND} -profile ${PROFILE_FILE}"
+else
+  echo "No color profile found"
+fi
+
+COMMAND="${COMMAND} ${INPUT_FILE} -profile ${SRGB_PROFILE_FILE}"
+COMMAND="${COMMAND} -thumbnail ${SIZE}"
+COMMAND="${COMMAND} -density ${DENSITY}" #http://www.imagemagick.org/script/command-line-options.php#density
+COMMAND="${COMMAND} ${REMAINING}"
+COMMAND="${COMMAND} ${OUTPUT_FILE}"
+
+# Execute
+${COMMAND}
+RC=$?
+
+if [ ${RC} -eq 0 ]
+then
+  echo "Successfully converted image and saved to ${OUTPUT_FILE}"
 else
   echo "No color profile found"
   echo "Failed to convert image"
 fi
 
-exit $?
+exit ${RC}
